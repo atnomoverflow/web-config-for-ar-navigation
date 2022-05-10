f146dbfa2b7f7435e55cec948413f6aa592062e2
atnomoverflow <montaser-abdmajid.zehri@enis.tn>
2022-05-10 19:15:26 +0100

    login page with some routing set-up and private route helper function

---
+++ src/utils/privateroute.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Route, Navigate } from 'react-router-dom';
+export const PrivateRoute = ({ component, isAuthenticated, ...rest }: any) => {
+    const routeComponent = (props: any) => (isAuthenticated ? React.createElement(component, props) : <Navigate to={{ pathname: '/login' }} />);
+    return <Route {...rest} render={routeComponent} />;
+};
